1fcff2b812ebadefcac9e508a8493b06a5aa23e7
Max Bloomfield <bloomm2@rpi.edu>
2021-08-10 15:23:07 -0400

    Trainer is working at alpha level.

--- src/trainer/src/network/layers.py
+++ src/trainer/src/network/layers.py
@@ -1,11 +1,11 @@
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+#os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.layers import Dropout, Dense, Input, Activation
 from tensorflow.keras import backend as K
-tf.debugging.set_log_device_placement(True)
+#tf.debugging.set_log_device_placement(True)
 
 supportedKeywords = ['units', 'rate', 'activation']
 
@@ -26,46 +26,3 @@ class variationalDropout(Dropout):
         return K.in_train_phase(dropped_inputs, inputs, training=self.training)
       return K.in_train_phase(dropped_inputs, inputs, training=training)
     return inputs
-
-"""
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
-x_train = x_train.astype("float32")/ 255.0
-x_test = x_test.astype("float32")/ 255.0
-
-model = keras.Sequential(
-  [
-    keras.Input(shape=(32, 32, 3)),
-    layers.Conv2D(32, (3, 3), padding='valid', activation='relu'),
-    layers.MaxPool2D(pool_size=(2, 2)),
-    layers.Conv2D(64, 3, activation='relu'),
-    layers.MaxPool2D(),
-    Dropout(0.1, training=True),
-    layers.Conv2D(128, 3, activation='relu'),
-    layers.Flatten(),
-    Dropout(0.1, training=True),
-    layers.Dense(64, activation='relu'),
-    Dropout(0.1, training=True),
-    layers.Dense(10),
-    layers.Dense(64, activation='relu'),
-    Dropout(0.1, training=True),
-    layers.Dense(10),
-    layers.Dense(64, activation='relu'),
-    Dropout(0.1, training=True),
-    layers.Dense(10),
-  ]
-)
-
-model.compile(
-  loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-  optimizer=keras.optimizers.Adam(lr=3.0e-4),
-  metrics=['accuracy'],
-)
-
-model.fit(x_train, y_train, batch_size=64, epochs=1, verbose=2)
-model.evaluate(x_test, y_test, batch_size=64, verbose=2)
-
-model.save("nn.h5")
-
-json_string = model.to_json(indent=1)
-print(json_string)
-"""
--- src/trainer/src/network/model.py
+++ src/trainer/src/network/model.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
 from tensorflow import keras
+from tensorflow.python.keras.optimizer_v2.adam import Adam
 import network.layers as layers
 from tensorflow.keras import backend as K
-from mko import MKO
 
 class Model(keras.Sequential):
 
@@ -13,12 +13,57 @@ class Model(keras.Sequential):
                           name="Inputs")]
 
       for ld in m.model_structure['layers']:
-        layerargs = {}
-        layerfunc = getattr(layers, ld['type'])
+        layer_args = {}
+        layer_func = getattr(layers, ld['type'])
         for k, v in ld.items():
           if k in layers.supportedKeywords:
-            layerargs[k] = v
+            layer_args[k] = v
         
-        seq.append(layerfunc(**layerargs))
+        seq.append(layer_func(**layer_args))
 
       super(Model, self).__init__(seq, **kwargs)
+
+      self._mko = m
+    
+  def compile(self, **kwargs):
+    
+    tc = self._mko.training_controls
+    if 'loss' not in kwargs:
+      kwargs['loss'] = keras.losses.MeanSquaredError(reduction='auto')
+    if 'optimizer' not in kwargs:
+      lr = tc['learning_rate']
+      kwargs['optimizer'] = Adam(lr=lr)
+    
+    super(Model,self).compile(**kwargs)
+
+  def train(self, **kwargs):
+
+    (x_train, y_train) = self._mko.training_data
+     
+    tc = self._mko.training_controls
+    if 'batch_size' not in kwargs:
+      if 'batch_size' in tc:
+        kwargs['batch_size'] = tc['batch_size']
+      else:
+        kwargs['batch_size'] = 64
+      
+    if 'epochs' not in kwargs:
+      if 'epochs' in tc:
+        kwargs['epochs'] = tc['epochs']
+      else:
+        kwargs['epochs'] = 1
+
+    super(Model, self).fit(x_train, y_train, **kwargs)
+
+  def evaluate(self, **kwargs):
+
+    (x_test, y_test) = self._mko.testing_data
+     
+    tc = self._mko.training_controls
+    if 'batch_size' not in kwargs:
+      if 'batch_size' in tc:
+        kwargs['batch_size'] = tc['batch_size']
+      else:
+        kwargs['batch_size'] = 64
+
+    super(Model, self).evaluate(x_test, y_test, **kwargs)

1f16114fcae691de849d42b7546de0ed98212568
Max Bloomfield <bloomm2@rpi.edu>
2022-03-10 14:35:46 -0500

    Frontend endpoints for training and inference managers

---
+++ src/frontend/server/Resources/Infer/routes.py
@@ -0,0 +1,16 @@
+
+from flask import Blueprint
+
+from server import app
+from server.wrappers import require_access, initialized
+from server.Resources.util import forward_request
+
+InferResource = Blueprint('Infer', __name__, url_prefix='/Infer')
+inference_manager_host = str(app.config.get('INFERENCE_MANAGER_HOST'))
+
+@InferResource.route('/sample', methods=['POST'])
+@initialized(True)
+@require_access('Infer')
+def infer_sample():
+    return forward_request(target_url=inference_manager_host +
+      "/sample")

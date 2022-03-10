ade6d5fe9c5fff9766f06e1b6697c08450dc10ef
Max Bloomfield <bloomm2@rpi.edu>
2022-03-10 14:35:46 -0500

    Frontend endpoints for training and inference managers

---
+++ src/frontend/server/Resources/Analyze/routes.py
@@ -0,0 +1,22 @@
+from flask import Blueprint
+
+from server import app
+from server.wrappers import require_access, initialized
+from server.Resources.util import forward_request
+
+AnalyzeResource = Blueprint('Analyze', __name__, url_prefix='/Analyze')
+inference_manager_host = str(app.config.get('INFERENCE_MANAGER_HOST'))
+
+@AnalyzeResource.route('/stat', methods=['POST'])
+@initialized(True)
+@require_access('Analyze')
+def Analyze_sample():
+    return forward_request(target_url=inference_manager_host +
+      "/stat")
+
+@AnalyzeResource.route('/integrate', methods=['POST'])
+@initialized(True)
+@require_access('Analyze')
+def Analyze_integrate():
+    return forward_request(target_url=inference_manager_host +
+      "/integrate")

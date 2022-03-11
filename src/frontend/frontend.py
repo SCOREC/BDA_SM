3d9abb3dd5246e64606c2bf0ef858f9dc50708c6
Max Bloomfield <bloomm2@rpi.edu>
2022-03-11 09:52:41 -0500

    Moved launchpoint

--- src/frontend/frontend.py
+++ src/frontend/frontend.py
@@ -1,3 +1,5 @@
+from os import getenv
+from threading import get_native_id
 from server import app, db
 from server.models import *
 from server.routes import *
@@ -6,3 +8,10 @@ from server.routes import *
 def make_shell_context():
     print('Registering shell context')
     return {'db': db, 'User': User, 'AuthToken': AuthToken, 'RefreshToken': RefreshToken}
+
+if __name__ == '__main__':
+    app.run(
+        host=getenv('IP', '0.0.0.0'),
+        port=getenv('FRONTEND_PORT', 8000),
+        debug=getenv('FRONTEND_DEBUG', False),
+    )

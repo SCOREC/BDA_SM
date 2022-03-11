9828291e92676712e1122d6e98ca0f3debb1ebd0
Max Bloomfield <bloomm2@rpi.edu>
2022-03-11 14:59:51 -0500

    Adding in Train and Infer resources

--- src/frontend/server/Resources/util.py
+++ src/frontend/server/Resources/util.py
@@ -3,17 +3,21 @@ import requests
 
 def forward_request(target_url,*args, **kwargs):
 
-   resp = requests.request(
-      method=request.method,
-      url=request.url.replace(request.host_url, target_url),
-      headers={key: value for (key, value) in request.headers if key != 'Host'},
-      data=request.get_data(),
-      cookies=request.cookies,
-      allow_redirects=False)
+   try:
+      resp = requests.request(
+         method=request.method,
+         url=request.url.replace(request.host_url, target_url),
+         headers={key: value for (key, value) in request.headers if key != 'Host'},
+         data=request.get_data(),
+         cookies=request.cookies,
+         allow_redirects=False)
 
-   excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
-   headers = [(name, value) for (name, value) in resp.raw.headers.items()
-            if name.lower() not in excluded_headers]
+      excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+      headers = [(name, value) for (name, value) in resp.raw.headers.items()
+               if name.lower() not in excluded_headers]
 
-   response = Response(resp.content, resp.status_code, headers)
+      response = Response(resp.content, resp.status_code, headers)
+   except Exception as e:
+      response = Response("Server unavailable", status=404)
+
    return response

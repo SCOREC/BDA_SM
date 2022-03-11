2b67a99f099de9f567a2d71c8ada9fb1f067254a
Max Bloomfield <bloomm2@rpi.edu>
2022-03-11 14:58:27 -0500

    Start up with app.run

--- src/frontend/frontend.py
+++ src/frontend/frontend.py
@@ -1,17 +1,9 @@
 from os import getenv
-from threading import get_native_id
-from server import app, db
-from server.models import *
-from server.routes import *
-
-@app.shell_context_processor
-def make_shell_context():
-    print('Registering shell context')
-    return {'db': db, 'User': User, 'AuthToken': AuthToken, 'RefreshToken': RefreshToken}
+from server import app
 
 if __name__ == '__main__':
     app.run(
         host=getenv('IP', '0.0.0.0'),
-        port=getenv('FRONTEND_PORT', 8000),
+        port=getenv('FRONTEND_PORT', 5000),
         debug=getenv('FRONTEND_DEBUG', False),
     )

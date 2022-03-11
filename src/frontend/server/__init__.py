4f3117aa5fa3640ac2b0fb3f607a17290d961664
Max Bloomfield <bloomm2@rpi.edu>
2022-03-11 14:58:27 -0500

    Start up with app.run

--- src/frontend/server/__init__.py
+++ src/frontend/server/__init__.py
@@ -1,16 +1,14 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
 
 app = Flask(__name__)
 app_settings = os.getenv('APP_SETTINGS', 'server.config.DevelopmentConfig')
 app.config.from_object(app_settings)
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-from server import routes
+from server import routes, models
 from server.Logger.models import Event
+db.create_all()
 
 app.config['SERVER_SESSION'] = os.urandom(24)
 
@@ -18,15 +16,19 @@ print("Starting with server_secret: '{}'".format(app.config.get('SERVER_SECRET')
 from server.Resources.Admin.routes import AdminResource
 app.register_blueprint(AdminResource)
 
+from server.Resources.Infer.routes import InferResource
+app.register_blueprint(InferResource)
+
 from server.Resources.Logging.routes import LoggingResource
 app.register_blueprint(LoggingResource)
+
+from server.Resources.Train.routes import TrainResource
+app.register_blueprint(TrainResource)
 
 from server.Resources.User.routes import UserResource
 app.register_blueprint(UserResource)
 
-"""
 ## Clear logs and log a server startup
 Event.query.delete()
 Event("SERVER", "Server started").logit()
 Event("SERVER", "Unique session ID: {}".format(app.config['SERVER_SESSION']) ).logit()
-"""

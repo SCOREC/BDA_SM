64e218b095ff034bfa1ae1f94ff20112ff69b27c
danielkopp4 <dan.r.kopp@gmail.com>
2022-04-14 04:03:51 -0400

    add analyzers

---
+++ src/analyzers/src/request_manager.py
@@ -0,0 +1,22 @@
+from typing import Iterable
+import requests
+
+class SamplerRequestManager:
+    def __init__(self, url: str, mko: str):
+        self.url = url
+        self.mko = mko
+
+    def get_sample(self, data: Iterable, samples: int):
+        params = {
+            "x": data,
+            "samples": samples
+        }
+
+        resp = requests.get(self.url, params=params)
+        
+        if resp.status_code != 200:
+            # throw error
+            pass
+
+        
+

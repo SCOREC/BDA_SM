cc86ff80abe4774f80d764a5fee9ce0b4dc74d13
Max Bloomfield <bloomm2@rpi.edu>
2022-08-09 21:36:26 -0400

    Docker build for frontend

--- src/frontend/Scripts/boot.sh
+++ src/frontend/Scripts/boot.sh
@@ -2,6 +2,5 @@
 source venv/bin/activate
 echo $PATH
 rm -f server/databases/*.db
-python testingscript.py
 
-exec gunicorn -b 0.0.0.0:5000  --access-logfile - --error-logfile -  authServer:app
+exec gunicorn -b 0.0.0.0:5000  --access-logfile - --error-logfile -  frontend:app

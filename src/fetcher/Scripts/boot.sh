f51f809ac1abcc33f5aafaac764b4ce213bed82d
Max Bloomfield <bloomm2@rpi.edu>
2022-08-11 20:47:16 -0400

    Move main app to fetcher.py and update Dockerfile to reflect

--- src/fetcher/Scripts/boot.sh
+++ src/fetcher/Scripts/boot.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 source venv/bin/activate
-echo $PATH
-pytest -v -s
 
-exec gunicorn -b 0.0.0.0:8000  --access-logfile - --error-logfile -  api:app
+exec gunicorn -b 0.0.0.0:5000  --access-logfile - --error-logfile -  fetcher:app

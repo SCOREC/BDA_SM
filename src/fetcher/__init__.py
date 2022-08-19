843f0aba1d7a819af758e7d6f43ed172da21742a
Max Bloomfield <bloomm2@rpi.edu>
2022-08-18 23:45:36 -0400

    Restructure flask app

---
+++ src/fetcher/__init__.py
@@ -0,0 +1,1 @@
+#fetcher/__init_.py
--- src/fetcher/fetcher.py
+++ src/fetcher/fetcher.py
@@ -2,7 +2,8 @@ from os import getenv
 from server import app
 
 if __name__ == '__main__':
-    app.run(
-        host=getenv('FETCHER_PORT', 5000),
-        debug=getenv('FETCHER_DEBUG', True)
-    )
+  app.run(
+    host=getenv('IP', '0.0.0.0'),
+    port=getenv('FETCHER_PORT', 5000),
+    debug=getenv('FETCHER_DEBUG', False),
+  )

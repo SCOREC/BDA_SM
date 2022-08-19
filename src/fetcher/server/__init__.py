0841e37852085837ec2a5c1dfa28ae1a460c476c
Max Bloomfield <bloomm2@rpi.edu>
2022-08-19 08:59:42 -0400

    Put HelloWorld in root not /api

--- src/fetcher/server/__init__.py
+++ src/fetcher/server/__init__.py
@@ -1,11 +1,14 @@
 import os
-from flask import Flask
+from flask import Flask, Blueprint, request
 
 app = Flask(__name__)
 app_settings = os.getenv('APP_SETTINGS', 'server.config.DevelopmentConfig')
 app.config.from_object(app_settings)
 
-from server import routes
-
 import server.resources.api
 app.register_blueprint(server.resources.api.bp, url_prefix='/api')
+
+@app.route('/helloWorld', methods = ['GET'])
+def imAlive():
+    request_data = request.args.get('query')  # type: ignore
+    return "Hello World!"

41a910acb7a95ab9472fe5b9c0f501c9aa57459a
Max Bloomfield <bloomm2@rpi.edu>
2022-08-23 23:16:17 -0400

    Missed one

--- src/resultCache/Tests/helpers.py
+++ src/resultCache/Tests/helpers.py
@@ -1,16 +1,33 @@
 import random
 import string
-from resultCache.config import TestConfig as config
+from Tests.base import app
+import server.file_daemon
+import server.claim_check as cc
+
+config = app.config
 
 def get_test_set():
-        usernames = generate_string(config.string_len_test, config.num_users_test)
-        ccs = generate_string(config.string_len_test, config.num_cc_test)
-        data = generate_string(config.num_characters_data_test, config.num_data_files)
-        return usernames, ccs, data
+  usernames = generate_string(config['STRING_LEN_TEST'], config['NUM_USERS_TEST'])
+  ccs = [cc.ClaimCheck(s).shortname for s in generate_string(config['STRING_LEN_TEST'], config['NUM_CC_TEST'])]
+  data = generate_string(config['NUM_CHARACTERS_DATA_TEST'], config['NUM_DATA_FILES'])
+  return usernames, ccs, data
 
 def generate_string(l, n):
-        strings = []
-        for _ in range(n):
-            gen_string = ''.join(random.choice(string.ascii_letters) for _ in range(l))
-            strings.append(gen_string)
-        return strings
+  strings = []
+  for _ in range(n):
+    gen_string = ''.join(random.choice(string.ascii_letters) for _ in range(l))
+    strings.append(gen_string)
+  return strings
+
+def bring_up_file_handler():
+  if server.file_daemon.file_handler is None:
+    server.file_daemon.file_handler = server.file_daemon.FileHandler(
+      app.config['MIN_EXPIRY_TIME'], app.config['MAX_EXPIRY_TIME'],
+      app.config['RATE_AVERAGE_WINDOW'], app.config['DIRECTORY']
+      )
+    pass
+
+def shut_down_file_handler():
+  if server.file_daemon.file_handler is not None:
+    server.file_daemon.file_handler.delete_all(True)
+    server.file_daemon.file_handler.end()

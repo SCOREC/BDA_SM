aa15f4e7f9b8ea2e127f2e15f5d76df62de06370
Max Bloomfield <bloomm2@rpi.edu>
2022-09-01 19:50:31 -0400

    Initial k8s deployment scripts

--- src/training_manager/Scripts/boot.sh
+++ src/training_manager/Scripts/boot.sh
@@ -2,7 +2,7 @@
 source venv/bin/activate
 
 export TRAINER_WORKING_DIRECTORY="./trainer/"
-export RESULTS_CACHE_URL="http://172.18.29.173:5002"
+export RESULTS_CACHE_URL="http:results-cache:5000/"
 export TRAINING_MANAGER_PORT=5000
-exec python3 training_manager.py
-#exec gunicorn -b 0.0.0.0:5000  --access-logfile - --error-logfile -  training_manager:app
+#exec python3 training_manager.py
+exec gunicorn -b 0.0.0.0:5000  --access-logfile - --error-logfile -  training_manager:app

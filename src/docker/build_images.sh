b1bef6a3b58f9e6cb1323f2c17b6e1aeebcafeb9
Max Bloomfield <bloomm2@rpi.edu>
2022-09-09 23:57:34 -0400

    New k8s yaml for IM and sampler

--- src/docker/build_images.sh
+++ src/docker/build_images.sh
@@ -16,6 +16,8 @@ while getopts "mM" options; do
 done
 
 ${BUILDER} build -t ${roottag}training_manager -f training_manager/Scripts/Dockerfile .
+${BUILDER} build -t ${roottag}inference_manager -f inference_manager/Scripts/Dockerfile .
+${BUILDER} build -t ${roottag}sampler -f sampler/Scripts/Dockerfile .
 (cd frontend; ${BUILDER} build -t ${roottag}frontend -f Scripts/Dockerfile .)
 (cd results_cache; ${BUILDER} build -t ${roottag}results_cache -f Scripts/Dockerfile .)
 (cd fetcher; ${BUILDER} build -t ${roottag}fetcher -f Scripts/Dockerfile .)

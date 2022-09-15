9bc6baf98c22b9b93ea6662d21e92a9e7d01da35
Max Bloomfield <bloomm2@rpi.edu>
2022-09-15 11:04:04 -0400

    Image registry to maxrpi

--- src/docker/build_images.sh
+++ src/docker/build_images.sh
@@ -3,9 +3,19 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd ${SCRIPT_DIR}/..
 
+TMver=":v1.0"
+IMver=":v1.0"
+Sver=":v1.0"
+FRver=":v1.0"
+RCver=":v1.0"
+FETver=":v1.0"
+
 BUILDER=docker
-while getopts "mM" options; do
+while getopts "DmM" options; do
   case "${options}" in
+    D)
+      roottag="maxrpi/"
+      ;;
     m)
       eval $(minikube docker-env)  
       roottag="mk/"
@@ -15,9 +25,9 @@ while getopts "mM" options; do
   esac
 done
 
-${BUILDER} build -t ${roottag}training_manager -f training_manager/Scripts/Dockerfile .
-${BUILDER} build -t ${roottag}inference_manager -f inference_manager/Scripts/Dockerfile .
-${BUILDER} build -t ${roottag}sampler -f sampler/Scripts/Dockerfile .
-(cd frontend; ${BUILDER} build -t ${roottag}frontend -f Scripts/Dockerfile .)
-(cd results_cache; ${BUILDER} build -t ${roottag}results_cache -f Scripts/Dockerfile .)
-(cd fetcher; ${BUILDER} build -t ${roottag}fetcher -f Scripts/Dockerfile .)
+${BUILDER} build -t ${roottag}training_manager${TMver} -f training_manager/Scripts/Dockerfile .
+${BUILDER} build -t ${roottag}inference_manager${IMver} -f inference_manager/Scripts/Dockerfile .
+${BUILDER} build -t ${roottag}sampler${Sver} -f sampler/Scripts/Dockerfile .
+(cd frontend; ${BUILDER} build -t ${roottag}frontend${FRver} -f Scripts/Dockerfile .)
+(cd results_cache; ${BUILDER} build -t ${roottag}results_cache${RCver} -f Scripts/Dockerfile .)
+(cd fetcher; ${BUILDER} build -t ${roottag}fetcher${FETver} -f Scripts/Dockerfile .)
